9a9b24ce0e84aa023ef1787ce585646a69ddeedc
55063001 <55063001@L815WD170037.edu.cbe.ab.ca>
2022-03-10 09:21:03 -0700

    updating ch2

---
+++ Chapter2/src/TicTacToe.java
@@ -0,0 +1,42 @@
+/*
+
+Program: TicTacToe.java          Last Date of this Revision: September 30, 2019
+
+Purpose: Create a TicTacToe board with an X in the center.
+
+Author: Dmitri Volcovschi
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+		
+		Player 'X', enter your move (row[1-3] column[1-3]): 2 2
+	
+		   |       |
+	       |       |
+		   |       |   
+   -------------------------
+		   |       | 
+	       |   X   |   
+	       |       |
+   -------------------------
+		   |       |   
+           |       |
+		   |       |
+	}
+
+}
+
+/* Screen Dump
+
+Hello World!
+ 
+ */
